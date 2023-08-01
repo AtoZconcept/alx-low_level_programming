b68feff04ef928fb3683b41bb79e90091959e691
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-08-01 02:19:15 -0700

    get_nodeint

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Function that returns the nth node of a listint_t linked list
+ * @head: First node in the in the linked list
+ * @index: index of the node, starting at 0
+ *
+ * Return: If the node does not exist, return NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int a = 0;
+	listint_t *temp = head;
+
+	while (temp && a < index)
+	{
+		temp = temp->next;
+		a++;
+	}
+	return (temp ? temp : NULL);
+}
+
