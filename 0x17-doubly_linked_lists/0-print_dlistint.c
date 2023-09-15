b24b7503f4e2f1ee1b7db55c7455778a14b99db3
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-14 20:55:27 -0700

    list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+include "lists.h"
+/**
+ * print_dlistint - print
+ * @h: pointer to the next list
+ *
+ * Return: length of the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		printf("\n");
+		i++;
+	}
+
+	return (i);
+}
