43de4e40e1137ef8858c971f3dbf860427beb4bc
AtoZconcept <opeyemiolaafeez.com>
2024-02-07 18:12:13 +0100

    add

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+/**
+ * linear_skip - function that searches for a value in a sorted array
+ * of integers using the linear_skip algorithm
+ * @value: value to search for
+ *
+ * Return: The first index where value is located
+*/
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *ex;
+	size_t l = list->index;
+
+	if (list == NULL)
+		return (NULL);
+	ex = list->express;
+	printf("Value checked at index [%lu] = [%d]\n", ex->index, ex->n);
+	while (ex != NULL && ex->n < value)
+	{
+		list = ex;
+		ex = ex->express;
+		if (ex != NULL)
+			printf("Value checked at index [%lu] = [%d]\n", ex->index, ex->n);
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", l, ex->index);
+	while (list != NULL && list->n <= value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", l, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+	return (NULL);
+}
